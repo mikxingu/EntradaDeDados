115acc781de5d3dd6ca00a925a97eb78e073956a
Michel Alves <michelalvs@gmail.com>
2021-05-13 20:54:59 -0300

    Configured input on new lines

--- src/App.java
+++ src/App.java
@@ -1,23 +1,26 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner (System.in);
-        String x;
-        int y;
-        double z;
-        
-        x = sc.next();
-        y = sc.nextInt();
-        z = sc.nextDouble();
+        String s1, s2, s3;
+        int x;
 
+        x = sc.nextInt();
+        sc.nextLine(); // Essa quebra de linha é necessária pra limpar a quebra de linha pendente quando se usa um "nextInt/nextDouble..."
+        
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        
+        System.out.println();
         System.out.println("Dados Digitados: ");
         System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
+        
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
 
         sc.close();
     }
